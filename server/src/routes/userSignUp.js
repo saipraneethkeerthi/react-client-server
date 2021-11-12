767bce7d7c8e540316fae79629d15402fe39a089
Keerthi Sai Praneeth <kpraneeth@innominds.com>
2021-11-12 23:07:52 +0530

    server req changes

--- server/src/routes/userSignUp.js
+++ server/src/routes/userSignUp.js
@@ -6,12 +6,7 @@ const router = new express.Router();
 
 const {validateEmail, validatePassword} = require("../middlewares/middlewares")
 
-router.get('/', (req, res)=>{
-   User.find({},(err,data)=>{
-       if(err) res.status(404).send(err)
-       else res.status(200).send(data)
-   })
-   })
+
 router.post('/user/create',validateEmail, validatePassword, (req, res)=>{
     console.log(req.body)
     User.insertMany(req.body).then((data)=> res.status(200).send(data))
